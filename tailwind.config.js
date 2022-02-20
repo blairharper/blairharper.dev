3e225f263eef4d5374ab339ebbcc1ad155e19036
Blair Harper <blair@passio.co.uk>
2022-02-20 14:27:25 +0000

    typography styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
@@ -10,6 +11,21 @@ module.exports = {
       colors: {
         'text-primary': colors.slate[900],
         'text-secondary': colors.slate[600],
+      },
+      zIndex: {
+        '-10': '-10',
+      },
+      fontFamily: {
+        sans: ['Matter', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        xl: '1.375rem', // 22px
+        '2xl': '1.5625rem', // 25px
+        '3xl': '1.875rem', // 30px
+        '4xl': '2.5rem', // 40px
+        '5xl': '3.125rem', // 50px
+        '6xl': '3.75rem', // 60px
+        '7xl': '4.375rem', // 70px
       },
     },
   },
